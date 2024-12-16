6d27c89976490c73ee73b66d24d48c0f22ded491
alBugankov <80395895+alBugankov@users.noreply.github.com>
2024-12-16 20:43:50 +0700

    Test-Task

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import StoreMain from "@/views/StoreMain.vue"
+</script>
+
+<template>
+  <header>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  </header>
+
+  <main>
+    <div class="container">
+      <div class="container__content">
+        <store-main/>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/modules/product-management/product-store/components/ProductStore.vue
@@ -0,0 +1,58 @@
+<script setup>
+const props = defineProps({
+  showStoreProduct: Function,
+});
+
+const addStoreProduct = (product) => {
+  props.showStoreProduct(product);
+}
+
+const storeItems = [
+  {
+    "id": 11,
+    "name": "Jacket 1"
+  },
+  {
+    "id": 12,
+    "name": "Jacket 2"
+  },
+  {
+    "id": 13,
+    "name": "Jacket 3"
+  },
+  {
+    "id": 14,
+    "name": "Jacket 4"
+  },
+  {
+    "id": 15,
+    "name": "Hoodie 1"
+  },
+  {
+    "id": 16,
+    "name": "Hoodie 2"
+  },
+  {
+    "id": 17,
+    "name": "Hoodie 3"
+  },
+  {
+    "id": 18,
+    "name": "Hoodie 4"
+  }
+]
+</script>
+
+<template>
+
+  <div class="store-container">
+    <div v-for="storeItem in storeItems" :key="storeItem.id" class="store-container__item"
+         @click="addStoreProduct(storeItem.name)"> {{ storeItem.name }}
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
